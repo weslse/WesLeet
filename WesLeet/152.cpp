c5c0aeb4e42f0a213e5c9128be76b97aa4e0ed69
weslee <weslse@hansung.ac.kr>
2020-07-30 16:08:02 +0900

    solve 2 problems

---
+++ WesLeet/152.cpp
@@ -0,0 +1,63 @@
+﻿/*
+ * Solved by Weslse
+ * 2020.07.30
+ */
+
+ // Problem
+ // 1184. Distance Between Bus Stops
+ // A bus has n stops numbered from 0 to n - 1 that form a circle. 
+ // We know the distance between all pairs of neighboring stops where distance[i] is the distance between the stops number i and (i + 1) % n.
+ // The bus goes along both directions i.e.clockwise and counterclockwise.
+ // Return the shortest distance between the given start and destination stops.
+
+ // RESULT
+ // Runtime: 4 ms, faster than 98.79% of C++ online submissions for Distance Between Bus Stops.
+ // Memory Usage: 8.9 MB, less than 23.53% of C++ online submissions for Distance Between Bus Stops.
+
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+	int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
+		// clockwise
+		int cw_minDist = 0;
+		int i = start;
+		while (true) {
+			if (i == destination)
+				break;
+	
+			cw_minDist += distance[i];
+			i++;
+			if (i >= distance.size())
+				i = 0;
+		}
+
+		// counterclockwise
+		int ccw_minDist = 0;
+		i = start;
+		while (true) {
+			if (i == destination)
+				break;
+
+			i--;
+			if (i < 0)
+				i = distance.size() - 1;
+			ccw_minDist += distance[i];
+		}
+		int minDist = min(cw_minDist, ccw_minDist);
+		return minDist;
+	}
+};
+
+int main() {
+	vector<int> d = { 1,2,3,4 };
+	int start = 0;
+	int dest = 3;
+	Solution s;
+	cout << s.distanceBetweenBusStops(d, start, dest) << endl;
+}
---
+++ WesLeet/543.cpp
@@ -0,0 +1,57 @@
+﻿/*
+ * Solved by Weslse
+ * 2020.07.30
+ */
+
+ // Problem
+ // 543. Diameter of Binary Tree
+ // Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+ // Example:
+ // Given a binary tree
+ //      1
+ //		/ \
+ //	   2   3
+ //	  / \
+ //	 4   5
+ // Return 3, which is the length of the path[4, 2, 1, 3] or [5, 2, 1, 3].
+
+ // Note: The length of path between two nodes is represented by the number of edges between them.
+
+ // RESULT
+ // Runtime: 56 ms, faster than 9.25% of C++ online submissions for Diameter of Binary Tree.
+ // Memory Usage: 20.9 MB, less than 23.88% of C++ online submissions for Diameter of Binary Tree.
+
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	int maxDepth(TreeNode* r) {
+		return r == nullptr ? 0 : max(maxDepth(r->left), maxDepth(r->right)) + 1;
+	}
+
+	int diameterOfBinaryTree(TreeNode* root) {
+		if (root == nullptr)
+			return 0;
+		int l = diameterOfBinaryTree(root->left);
+		int r = diameterOfBinaryTree(root->right);
+		int d = maxDepth(root->left) + maxDepth(root->right);
+		return max(l, max(r, d));
+	}
+};
+
